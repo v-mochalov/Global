be99ede453646ae1050e0d5b0d1da6d9d9218df2
v-mochalov <deathguuun@gmail.com>
2023-05-25 21:48:28 +0300

    add turnillet

--- build/js/bundle.js
+++ build/js/bundle.js
@@ -147,8 +147,7 @@ document.querySelectorAll('a[href^="#"]').forEach(function (anchor) {
 $(document).ready(function () {
   $('.block__title').click(function (event) {
     if ($('.block').hasClass('one')) {
-      $('.block__title').not($(this)).removeClass('active');
-      $('.block__text').not($(this).next()).slideUp(300);
+      $('.block__title').not($(this)).removeClass('active'); // $('.block__text').not($(this).next()).slideUp(300);
     }
 
     $(this).toggleClass('active').next().slideToggle(300);
--- src/js/index.js
+++ src/js/index.js
@@ -154,7 +154,7 @@ $(document).ready(function () {
 	$('.block__title').click(function (event) {
 		if ($('.block').hasClass('one')) {
 			$('.block__title').not($(this)).removeClass('active');
-			$('.block__text').not($(this).next()).slideUp(300);
+			// $('.block__text').not($(this).next()).slideUp(300);
 		}
 		$(this).toggleClass('active').next().slideToggle(300);
 	});
